f43386a6efcfa12f93a015bc0d6b0405d8101004
reva.ma@phystech.edu <reva.ma@phystech.edu>
2019-12-18 01:15:40 +0300

    2

--- gamefile.py
+++ gamefile.py
@@ -1,39 +1,63 @@
-from tkinter import *
+import tkinter as tk
 from random import *
 
-root = Tk()
-fr = Frame(root)
+root = tk.Tk()
+fr = tk.Frame(root)
 root.geometry('800x600')
-canv =Canvas(root, bg='white')
-canv.pack(fill=BOTH, expand=1)
+canv = tk.Canvas(root, bg='white')
+canv.pack(fill=tk.BOTH, expand=1)
 width = 800
 height = 600
+HP = 10
 num_of_lines = 8
 line_width = width/num_of_lines
-block_height = 5
+block_height = 30
+button_height = 30
+
 class Line:
 	def __init__(self, x):
 		self.x = x
 
 class Block:
 	def __init__(self, x, block_height, vy):
-		self.x = x
+		self.x = x - 1
 		self.y = 0
 		self.vy = vy
-		self.block_height = block_height
+		self.height = block_height
 		self.life = True
-		self.color = randrange(1, 6, 1)
-		if self.color == 1 :
-			self.id = canv.create_rectangle(self.x, self.y, self.x - line_width, self.y + block_height, fill = 'black')
-		else :
-			self.id = canv.create_rectangle(self.x, self.y, self.x - line_width, self.y + block_height, fill = 'white')				
+#		self.color = randrange(1, 6, 1)
+#		if self.color == 1 :
+		self.id = canv.create_rectangle(self.x, self.y, self.x - line_width + 1, self.y + self.height, fill = 'white')
+#		else :
+#			self.id = canv.create_rectangle(self.x, self.y, self.x - line_width, self.y + self.height, fill = 'white')				
 	def move(self):
-		self.y += self.vy
-		canv.move(self.id, 0, self.vy)
+		if self.life == True:
+			self.y += self.vy
+			canv.move(self.id, 0, self.vy)
+		else:
+			canv.delete(self.id)
+			del self
+
+class Button:
+	def __init__(self, x, button_height):
+		self.x = x
+		self.y = height - button_height
+		self.height = button_height
+		self.actionBtn = tk.Button(root,activeforeground = "white", borderwidth = 1, bg = "black", command = self.clicked).place(x = self.x, y = self.y, width = line_width, height = self.height)
+#		self.id = canv.create_rectangle(self.x, self.y, self.x - line_width, self.y + self.height, fill = 'red')
+	def clicked(self):
+		found = 0
+		for block in blocks:
+			if block.x == self.x + line_width - 1:
+				block.life = False
+				found = 1
+				continue
+		if found == 1:
+			HP -= 1
 
 lines = [Line(i*line_width) for i in range(1, num_of_lines)]
-
 blocks = [Block(lines[i-1].x, block_height * i, i/10) for i in range(1, num_of_lines)]
+buttons = [Button(lines[i-1].x - line_width, button_height) for i in range(1, num_of_lines)]
 
 def upd(event=''):
 	for block in blocks :
@@ -43,4 +67,4 @@ def upd(event=''):
 
 upd()
 
-mainloop()
+tk.mainloop()
