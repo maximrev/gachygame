4c813dacbacc5ec7eb1791a0ab81edfc36250bd1
reva.ma@phystech.edu <reva.ma@phystech.edu>
2019-12-19 03:13:54 +0300

    buttons

--- gamefile.py
+++ gamefile.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from random import *
+from pynput import keyboard
+from Block import *
+from pynput.keyboard import Key, Listener
+import threading
+import os
+
 
 root = tk.Tk()
 fr = tk.Frame(root)
@@ -13,6 +19,9 @@ num_of_lines = 8
 line_width = width/num_of_lines
 block_height = 30
 button_height = 30
+keys = [i for i in range(1, num_of_lines)]
+pressed = [0 for i in range(1, num_of_lines)]
+
 
 class Line:
 	def __init__(self, x):
@@ -47,17 +56,20 @@ class Button:
 	def __init__(self, line, x, button_height):
 		self.x = x
 		self.y = height - button_height
-		self.line = line
+		self.line = int(line)
 		self.height = button_height
-		self.actionBtn = tk.Button(root, activeforeground = "white", borderwidth = 1, bg = "black", command = self.clicked).place(x = self.x, y = self.y, width = line_width, height = self.height)
+		self.actionBtn = tk.Button(root, text = self.line + 1, fg = 'red', activeforeground = "white", borderwidth = 1, bg = "black", command = self.clicked)
+		self.actionBtn.place(x = self.x, y = self.y, width = line_width, height = self.height)
 	def clicked(self):
+		buf = 0
 		for block in blocks:
 			if block.line == self.line:
 				if block.y + block.height >= self.y and block.y <= self.y:
 					block.life = False
-					continue
-		global HP
-		HP -= 1
+					buf = 1
+		if buf == 0:
+			global HP
+			HP -= 1
 
 class HP_Bar:
 	def __init__(self, x, y):
@@ -72,12 +84,29 @@ class HP_Bar:
 		self.text_var.set(HP)
 		
 def lose():				#change it
-	quit()
+	os._exit(1)
 
-hp_bar = HP_Bar(0, 0)
-lines = [Line(i*line_width) for i in range(1, num_of_lines)]
-blocks = [Block(i-1, lines[i-1].x, block_height * i, i/10) for i in range(1, num_of_lines)]
-buttons = [Button(i-1, lines[i-1].x - line_width, button_height) for i in range(1, num_of_lines)]
+def on_press(key):
+	for i in range(1, num_of_lines):
+		if "'" + str(keys[i-1]) + "'" == str(key):
+			if pressed[i-1] == 0:
+				buttons[i-1].clicked()
+				buttons[i-1].actionBtn.config(bg='white')
+				pressed[i-1] = 1
+
+
+def on_release(key):
+	for i in range(1, num_of_lines):
+		if "'" + str(keys[i-1]) + "'" == str(key):
+			if pressed[i-1] == 1:
+				buttons[i-1].actionBtn.config(bg='black')
+				pressed[i-1] = 0
+	if key == Key.esc:
+        	os._exit(1)
+
+def start_capture():
+	with Listener(on_press=on_press, on_release=on_release) as listener:
+		listener.join()
 
 def upd(event=''):
 	for block in blocks :
@@ -85,6 +114,15 @@ def upd(event=''):
 	hp_bar.upd()
 	canv.update()
 	root.after(1, upd)
+
+hp_bar = HP_Bar(0, 0)
+
+lines = [Line(i*line_width) for i in range(1, num_of_lines)]
+blocks = [Block(i-1, lines[i-1].x, block_height * i, i/10) for i in range(1, num_of_lines)]
+buttons = [Button(i-1, lines[i-1].x - line_width, button_height) for i in range(1, num_of_lines)]
+
+thread1 = threading.Thread(target = start_capture)
+thread1.start()
 
 upd()
 
